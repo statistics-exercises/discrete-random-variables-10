d573bf2d2a75848dc3e301caf57e4f0269b0f2cb
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 19:08:42 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# You may wish to write some code here
+
+def negative_binomial(k, p) :
+  # Your code for generating a negative binomial random variable goes here
+  
+# Your code for generating the list of negative binomial random variables goes here
+kval, prob = 4, 0.5
+  
+# This will plot a graph showing your negative binomial random variables.
+plt.plot( indices, random_variables, ".") 
+plt.savefig("negave_binomial_rvs.png")
